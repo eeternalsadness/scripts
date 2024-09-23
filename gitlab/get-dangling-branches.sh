a0e24bca4887bbbf7d7bc86feb6b1302f518aea9
Bach Nguyen <69bnguyen@gmail.com>
2024-09-23 22:19:27 +0700

    fix get-dangling-branches

--- gitlab/get-dangling-branches.sh
+++ gitlab/get-dangling-branches.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 list_projects() {
     local page=$1
     local items_per_page=$2
     curl -s --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
-        "https://$GITLAB_HOST/api/v4/projects?membership=true&page=$page&per_page=$items_per_page"
+        "https://$GITLAB_HOST/api/v4/projects?membership=true&simple=true&page=${page}&per_page=${items_per_page}"
 }
 
 get_all_project_ids() {
@@ -27,43 +27,45 @@ get_all_project_ids() {
     echo "$project_ids"
 }
 
-list_branches() {
+get_recent_project_ids() {
+    local page=1
+    local items_per_page=100
+
+    curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
+        "https://$GITLAB_HOST/api/v4/events?action=pushed?per_page=100" | jq -r '.[] | .project_id' | sort | uniq
+}
+
+list_recent_branches() {
     local project_id=$1
-    local page=$2
-    local items_per_page=$3
+    local items_per_page=$2
     curl -s --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
-        "https://$GITLAB_HOST/api/v4/projects/${project_id}/repository/branches?page=$page&per_page=$items_per_page"
+        "https://$GITLAB_HOST/api/v4/projects/${project_id}/repository/branches?sort=updated_desc&page=1&per_page=${items_per_page}"
 }
 
 get_all_user_branches_in_project() {
     local project_id=$1
     local email=$2
-    local page=1
-    local items_per_page=100
+    local items_per_page=10
 
     local branches=()
-    while true; do
-        result=($(list_branches $project_id $page $items_per_page | jq -r --arg email "$email" '.[] | select(.commit.author_email == $email and .default == false) | .name'))
 
-        if [ "${#result}" -eq 0 ]; then
-            break
-        fi
+    branches_on_page=$(list_recent_branches "$project_id" "$items_per_page")
 
-        branches+=("${result[@]}")
-        ((page++))
-    done
+    if [ -n "$branches_on_page" ]; then
+        branches=($(jq -r --arg email "$email" '.[] | select(.commit.author_email == $email and .default == false) | .name' <<<"$branches_on_page"))
+    fi
 
     echo "${branches[*]}"
 }
 
 get_all_dangling_branches_in_project() {
     local project_id=$1
-    local branches=($2)
+    local branches=("$2")
     local page=1
     local items_per_page=100
 
     local dangling_branches=()
-    for branch in ${branches[@]}; do
+    for branch in "${branches[@]}"; do
         open_mr_for_branch=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
             "https://$GITLAB_HOST/api/v4/projects/${project_id}/merge_requests?scope=created_by_me&state=opened&source_branch=$branch")
 
@@ -72,7 +74,7 @@ get_all_dangling_branches_in_project() {
         fi
     done
 
-    echo ${dangling_branches[*]}
+    echo "${dangling_branches[*]}"
 }
 
 get_open_mrs() {
@@ -95,17 +97,29 @@ get_user_id() {
 #    python3 -c "import urllib.parse; print(urllib.parse.quote('$string'))"
 #}
 
-projects=$(get_all_project_ids)
+#get_all_project_ids
 user_email=$(get_user_email)
+echo "Getting all projects that '$user_email' is a member of..."
+projects=$(get_all_project_ids)
 
-echo "$projects" | jq -c '.[]' | while IFS= read -r project; do
+project_list=$(echo "$projects" | jq -c '.[]')
+all_dangling_branches=()
+while IFS= read -r project; do
     project_id=$(echo "$project" | jq -r '.id')
     project_name=$(echo "$project" | jq -r '.name_with_namespace')
 
-    branches=($(get_all_user_branches_in_project $project_id $user_email))
-    dangling_branches=($(get_all_dangling_branches_in_project $project_id ${branches[*]}))
+    #echo "Getting all branches created by '$user_email' in '$project_name'"
+    #get_all_user_branches_in_project "$project_id" "$user_email"
+    branches=($(get_all_user_branches_in_project "$project_id" "$user_email"))
+    echo "Getting all branches created by '$user_email' without an open MR in '$project_name'"
+    dangling_branches=($(get_all_dangling_branches_in_project "$project_id" "${branches[*]}"))
 
-    for dangling_branch in ${dangling_branches[@]}; do
-
+    for dangling_branch in "${dangling_branches[@]}"; do
+        all_dangling_branches+=("$project_name,$dangling_branch")
     done
+done <<<"$project_list"
+
+echo "Found ${#all_dangling_branches[@]} dangling branches:"
+for branch in "${all_dangling_branches[@]}"; do
+    echo "$branch"
 done
