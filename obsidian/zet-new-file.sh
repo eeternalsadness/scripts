6dbc5203c0bed5a2f98ad2777485cb9044905dcb
Bach Nguyen <69bnguyen@gmail.com>
2024-11-12 21:42:40 +0700

    update obsidian script to handle spaces in file names

--- obsidian/zet-new-file.sh
+++ obsidian/zet-new-file.sh
@@ -3,16 +3,16 @@
 file_name=""
 
 if [[ -n $1 ]]; then
-  file_name=$1
+    file_name=$1
 else
-  read -rp "Enter the file name: " file_name
+    read -rp "Enter the file name: " file_name
 fi
 
 file_name="${file_name}.md"
 
 # create file only if it doesn't exist
-if [[ ! -f $OBSIDIAN_INBOX/$file_name ]]; then
-  cat <<EOT >$OBSIDIAN_INBOX/$file_name
+if [[ ! -f "$OBSIDIAN_INBOX/$file_name" ]]; then
+    cat >"$OBSIDIAN_INBOX/$file_name" <<EOT
 
 
 ---
@@ -27,5 +27,5 @@ fi
 
 # no file name provided, open neovim
 if [[ -z $1 ]]; then
-  nvim -c "cd $OBSIDIAN" $OBSIDIAN_INBOX/$file_name
+    nvim -c "cd $OBSIDIAN" "$OBSIDIAN_INBOX/$file_name"
 fi
