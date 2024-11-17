18c22181653ec7cc1097a558030201da1f5ffa92
Bach Nguyen <69bnguyen@gmail.com>
2024-11-17 16:03:23 +0700

    add yearly note

---
+++ obsidian/yearly-note.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+file_name="$(date +"%Y").md"
+file_path="$OBSIDIAN/Periodic Notes/3-Yearly/$file_name"
+
+# create file only if it doesn't exist
+if [[ ! -f "$file_path" ]]; then
+    cat >"$file_path" <<EOT
+# ${file_name}
+
+<< [[$(date -d "last year" +"%Y-%m")]] | [[$(date -d "next year" +"%Y-%m")]] >>
+
+## $(date +"%Y") Resolutions
+
+- [ ] Resolution #1
+- [ ] Resolution #2
+
+## $(date +"%Y") Recap
+
+## Reflections
+
+### What did you accomplish this year?
+
+### What made you proud?
+
+### What could you have done better?
+
+### What are some of the notable things that happened this year?
+
+---
+
+$(date +'%Y%m%d%H%M')
+
+Tags:
+
+Links:
+EOT
+else
+    echo "Yearly note '$file_name' already exists"
+fi
