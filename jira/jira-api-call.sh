f825d50008a0a7f5ffdef86135a178d56abe0724
Bach Nguyen <69bnguyen@gmail.com>
2025-01-24 16:59:09 +0700

    init jira go module

--- jira/jira-api-call.sh
+++ jira/jira-api-call.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-header="Accept: application/json"
-method="$1"
-api_path="$2"
+if [[ $# -eq 1 ]]; then
+  method="GET"
+  api_path="$1"
+elif [[ $# -eq 2 ]]; then
+  method="$1"
+  api_path="$2"
+else
+  echo "Must have 1 or 2 arguments!"
+  exit 1
+fi
 
 url="https://${JIRA_DOMAIN}/rest/api/3/${api_path}"
+auth=$(echo -n "${JIRA_EMAIL}:${JIRA_API_TOKEN}" | base64)
 echo "$url"
 
-curl -fsSL --request "$method" \
-  --url "$url" \
-  --user "${JIRA_EMAIL}:${JIRA_API_TOKEN}" \
-  --header "$header" |
+curl -fsSL -X "$method" \
+  -H "Authorization: Basic $auth" \
+  -H "Accept: application/json" \
+  "$url" |
   jq
