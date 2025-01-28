3d10adac89dbbd455b8fd1340760749f75266264
Bach Nguyen <69bnguyen@gmail.com>
2025-01-28 17:15:34 +0700

    add GetAuthToken() for config

--- jira/util/config.go
+++ jira/util/config.go
@@ -1,7 +1,17 @@
 package util
 
+import (
+  "encoding/base64"
+  "fmt"
+)
+
 type Config struct {
   domain string
   email string
   apiToken string
+}
+
+func (config *Config) GetAuthToken() string {
+  auth := fmt.Sprintf("%s:%s", config.email, config.apiToken)
+  return base64.StdEncoding.EncodeToString([]byte(auth))
 }
---
+++ jira/util/jira.go
@@ -0,0 +1,9 @@
+package util
+
+import (
+  "fmt"
+)
+
+func CallApi(url string, path string, method string, authToken string) {
+  return
+}
