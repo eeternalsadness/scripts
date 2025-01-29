2ae664788f15d88fff1fd67b1f1188c3c42ac42d
Bach Nguyen <69bnguyen@gmail.com>
2025-01-29 23:44:16 +0700

    update command description for get

--- jira/cmd/get.go
+++ jira/cmd/get.go
@@ -28,13 +28,8 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get a Jira resource",
+	//Long: `Get a Jira resource`,
 }
 
 func init() {
