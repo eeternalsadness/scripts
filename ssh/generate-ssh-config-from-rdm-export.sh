6e39e208a25d15520a63bc9e6e0a9a58168e1393
Bach Nguyen <69bnguyen@gmail.com>
2025-02-18 17:17:26 +0700

    refactor gen ssh config script to make config gen optional

--- ssh/generate-ssh-config-from-rdm-export.sh
+++ ssh/generate-ssh-config-from-rdm-export.sh
@@ -8,47 +8,45 @@ echo "- sshpass installed"
 echo "- jq installed"
 echo "=============================================================================="
 
-if [[ -z "$1" ]]; then
-  echo "Please enter the path to the exported json file!"
-  echo "USAGE: sshgenconfig [exported_json_file_path]"
-  exit 1
-fi
-
 read -rp "Please enter the name of the output file: " file_name
-
-ssh_dir="$SSH_CONFIG_DIR"
 file_path="$SSH_CONFIG_DIR/$file_name"
-if [[ -f "$file_path" ]]; then
-  echo "File '$file_name' already exists at '$file_path'!"
-  exit 1
+
+read -rp "Generate ssh config file? [y/n]: " generate_config
+if [[ "$generate_config" == "y" ]]; then
+  read -rp "Please enter the path to the exported json file: " json_file
+  if [[ -f "$file_path" ]]; then
+    echo "File '$file_name' already exists at '$file_path'!"
+    exit 1
+  fi
+
+  domain_name="citigo.io"
+  hosts=$(jq '[ .Connections[] | select(.ConnectionType == 77) | { Name: .Name, Group: .Group } ]' "$json_file")
+
+  echo "Generating ssh config file..."
+  touch "$file_path"
+  for ((i = 0; i < $(jq -r 'length' <<<"$hosts"); i++)); do
+    host=$(jq --argjson i "$i" -r '.[$i].Name' <<<"$hosts")
+    group=$(jq --argjson i "$i" -r '.[$i].Group' <<<"$hosts")
+    hostname="${host}.${domain_name}"
+    if [[ "$group" != "null" ]]; then
+      host="${group}/${host}"
+    fi
+    cat >>"$file_path" <<EOT
+Host ${host}
+    HostName ${hostname}
+    StrictHostKeyChecking no
+
+EOT
+  done
+  echo "Config file generated at '$file_path'."
 fi
 
 read -rp "Do you want to automatically add the public key to the exported hosts? (Enter 'yes' to confirm): " auto_add_pub_key
 if [[ "$auto_add_pub_key" == "yes" ]]; then
   read -rsp "Enter the SSH password: " ssh_password
   echo -e "\n"
-fi
-
-domain_name="citigo.io"
-hosts=$(jq '[ .Connections[] | select(.ConnectionType == 77) | { Name: .Name, Group: .Group } ]' "$1")
-
-touch $file_path
-for ((i = 0; i < $(jq -r 'length' <<<"$hosts"); i++)); do
-  host=$(jq --argjson i "$i" -r '.[$i].Name' <<<"$hosts")
-  group=$(jq --argjson i "$i" -r '.[$i].Group' <<<"$hosts")
-  hostname="${host}.${domain_name}"
-  if [[ "$group" != "null" ]]; then
-    host="${group}/${host}"
-  fi
-  cat >>"$file_path" <<EOT
-Host ${host}
-    HostName ${hostname}
-    StrictHostKeyChecking no
-
-EOT
-
-  if [[ "$auto_add_pub_key" == "yes" ]]; then
+  for host in $(cat "$file_path" | grep '^Host' | sed 's/^Host //'); do
     echo "Adding public key to '$host'"
-    sshpass -p $ssh_password ssh-copy-id -o StrictHostKeyChecking=no "$host"
-  fi
-done
+    sshpass -p "$ssh_password" ssh-copy-id -o StrictHostKeyChecking=no "$host"
+  done
+fi
