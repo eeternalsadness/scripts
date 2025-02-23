a7837639b60a9cc63bf952ffde6ad1d75f341a95
Bach Nguyen <69bnguyen@gmail.com>
2025-02-23 12:28:51 +0700

    handle custom brightness

--- asusctl/toggle-keyboard-backlight.sh
+++ asusctl/toggle-keyboard-backlight.sh
@@ -6,10 +6,22 @@ if [[ ! -f "$kb_backlight_file" ]]; then
   touch "$kb_backlight_file"
 fi
 
-if [[ $(cat "$kb_backlight_file") -eq 1 ]]; then
-  asusctl -k off
-  echo "0" >"$kb_backlight_file"
+# handle custom brightness
+if [[ -n "$1" ]]; then
+  if [[ ! "$1" =~ ^(off|low|med|high)$ ]]; then
+    echo "Invalid keyboard brightness '$1'!"
+    exit 1
+  fi
+
+  asusctl -k "$1"
+  echo "$1" >"$kb_backlight_file"
 else
-  asusctl -k low
-  echo "1" >"$kb_backlight_file"
+  if [[ $(cat "$kb_backlight_file") != "off" ]]; then
+    asusctl -k off
+    echo "off" >"$kb_backlight_file"
+  else
+    # use low as default
+    asusctl -k low
+    echo "low" >"$kb_backlight_file"
+  fi
 fi
