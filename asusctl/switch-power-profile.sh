079321e4888de59f468a895ec78a5be8a5ba12c0
Bach Nguyen <69bnguyen@gmail.com>
2025-03-01 14:57:38 +0700

    update the supergfxd.conf file when switching graphics mode

--- asusctl/switch-power-profile.sh
+++ asusctl/switch-power-profile.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 hyprland_config_file="$HOME/.config/hypr/hyprland.conf"
+supergfxd_config_file="/etc/supergfxd.conf"
 
 case "$1" in
 "low")
   # NOTE: on battery, maximum power saving
   asusctl profile -P Quiet
   supergfxctl -m Integrated
+  sed -i 's/"mode": ".*"/"mode": "Integrated"/' "$supergfxd_config_file"
   brightnessctl -c backlight s 20%
   sed -i 's/^monitor=.*/monitor=,2560x1600@60.00,auto,auto/' "$hyprland_config_file"
   ;;
@@ -14,6 +16,7 @@ case "$1" in
   # NOTE: on AC power, moderate performance with low fan noise
   asusctl profile -P Quiet
   supergfxctl -m Hybrid
+  sed -i 's/"mode": ".*"/"mode": "Hybrid"/' "$supergfxd_config_file"
   brightnessctl -c backlight s 50%
   sed -i 's/^monitor=.*/monitor=,2560x1600@120.00,auto,auto/' "$hyprland_config_file"
   ;;
@@ -21,6 +24,7 @@ case "$1" in
   # NOTE: on AC power, max performance with max fan noise
   asusctl profile -P Balanced
   supergfxctl -m Hybrid
+  sed -i 's/"mode": ".*"/"mode": "Hybrid"/' "$supergfxd_config_file"
   brightnessctl -c backlight s 50%
   sed -i 's/^monitor=.*/monitor=,2560x1600@120.00,auto,auto/' "$hyprland_config_file"
   ;;
