663224bde185cd7427002ae1e79e335ec45bf2c8
Bach Nguyen <69bnguyen@gmail.com>
2025-04-19 15:41:29 +0700

    add commands to set up bluetooth & brightnessctl

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -17,9 +17,13 @@ sudo pacman -S --needed pipewire wireplumber
 
 # bluetooth
 sudo pacman -S --needed bluez bluez-utils
+systemctl enable --now bluetooth.service
 
 # power management
 sudo pacman -S --needed cpupower brightnessctl
+# allow non-root user to change brightness
+usermod -aG video bach
+cat 'ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"' >/etc/udev/rules.d/backlight.rules
 
 # screenshot
 sudo pacman -S --needed grim slurp
@@ -29,9 +33,6 @@ sudo pacman -S --needed wl-clipboard
 
 # qutebrowser
 sudo pacman -S --needed qutebrowser
-
-# misc
-sudo pacman -S --needed wikiman
 
 ##############################################
 # Next steps
