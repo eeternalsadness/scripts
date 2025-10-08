c8e32ac1e451347c40b326fdd86b8985c0ab66c8
Bach Nguyen <69bnguyen@gmail.com>
2025-10-08 11:09:48 +0800

    redirect echo to stderr

--- tmux/new-tmux-session.sh
+++ tmux/new-tmux-session.sh
@@ -11,6 +11,6 @@ session_name=$(new-workspace)
 
 # switch to session
 if [[ -n "$session_name" ]]; then
-  echo "Switching to session '$session_name'"
+  echo "Switching to session '$session_name'" >&2
   tmux switch -t "$session_name"
 fi
