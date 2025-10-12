3cf85fe921b47a870ca1117d91099d0e4c21b7f0
Bach Nguyen <69bnguyen@gmail.com>
2025-10-12 17:35:01 +0800

    change symlink-dotfiles.sh to a standalone script instead of a function to be sourced

--- dotfiles/symlink-dotfiles.sh
+++ dotfiles/symlink-dotfiles.sh
@@ -1,80 +1,80 @@
-symlink_dotfiles() {
-  local dotfiles_dir=${1:-$REPO/personal/dotfiles}
-  local scripts_dir=${2:-$SCRIPTS}
+#!/bin/bash
 
-  source "$scripts_dir/common/echo.sh"
+dotfiles_dir=${1:-$REPO/personal/dotfiles}
+scripts_dir=${2:-$SCRIPTS}
 
-  # show warning and prompt for confirmation
-  echo_warning "This script will symlink all dotfiles in $dotfiles_dir to the home directory ($HOME)."
-  echo_warning "All directories in $dotfiles_dir/.config/ will also be symlinked to $HOME/.config/."
-  echo_warning "All files and directories will be backed up (with versioning) by default."
+source "$scripts_dir/common/echo.sh"
 
-  read -rp "Proceed? [y/n]: " user_input
-  case "$user_input" in
-  "y") ;;
-  "n") return 1 ;;
-  *)
-    echo_error "Invalid input: '$user_input'."
-    return 1
-    ;;
-  esac
+# show warning and prompt for confirmation
+echo_warning "This script will symlink all dotfiles in $dotfiles_dir to the home directory ($HOME)."
+echo_warning "All directories in $dotfiles_dir/.config/ will also be symlinked to $HOME/.config/."
+echo_warning "All files and directories will be backed up (with versioning) by default."
 
-  # symlink dotfiles
-  echo "Linking dotfiles..."
+read -rp "Proceed? [y/n]: " user_input
+case "$user_input" in
+"y") ;;
+"n") return 1 ;;
+*)
+  echo_error "Invalid input: '$user_input'."
+  return 1
+  ;;
+esac
 
-  for file in "$dotfiles_dir"/.*; do
-    file_name=$(basename "$file")
+# symlink dotfiles
+echo "Linking dotfiles..."
 
-    # Ignore folders & special files
-    if [[ -d "$file" ]] || [[ "$file_name" == ".gitignore" ]]; then
-      continue
-    fi
+for file in "$dotfiles_dir"/.*; do
+  file_name=$(basename "$file")
 
-    target="$HOME/$file_name"
-
-    # create symlink with versioned backup
-    set +e
-    symlink_target=$(readlink "$target")
-    set -e
-    if [[ "$symlink_target" == "$file" ]]; then
-      echo "Symlink '$target' -> '$symlink_target' already exists!"
-    else
-      echo "Creating symlink for '$file' at '$target'"
-      VERSION_CONTROL=t ln -sbv "$file" "$target"
-    fi
-  done
-
-  # symlink dirs in .config/
-  if [[ ! -d "$HOME/.config" ]]; then
-    mkdir "$HOME/.config"
+  # Ignore folders & special files
+  if [[ -d "$file" ]] || [[ "$file_name" == ".gitignore" ]]; then
+    continue
   fi
 
-  for file in "$dotfiles_dir"/.config/*; do
-    file_name=$(basename "$file")
+  target="$HOME/$file_name"
 
-    # ignore special files
-    if [[ "$file_name" == ".git" || "$file_name" == ".gitignore" ]]; then
-      continue
-    fi
+  # create symlink with versioned backup
+  set +e
+  symlink_target=$(readlink "$target")
+  set -e
+  if [[ "$symlink_target" == "$file" ]]; then
+    echo "Symlink '$target' -> '$symlink_target' already exists!"
+  else
+    echo "Creating symlink for '$file' at '$target'"
+    VERSION_CONTROL=t ln -sbv "$file" "$target"
+  fi
+done
 
-    target="$HOME/.config/$file_name"
+# symlink dirs in .config/
+if [[ ! -d "$HOME/.config" ]]; then
+  mkdir "$HOME/.config"
+fi
 
-    # back up target dir if it exists
-    if [[ -d "$target" ]]; then
-      VERSION_CONTROL=t mv -bTv "$target" "${target}.backup"
-    fi
+for file in "$dotfiles_dir"/.config/*; do
+  file_name=$(basename "$file")
 
-    # create symlink
-    set +e
-    symlink_target=$(readlink "$target")
-    set -e
-    if [[ "$symlink_target" == "$file" ]]; then
-      echo "Symlink '$target' -> '$symlink_target' already exists!"
-    else
-      echo "Creating symlink for '$file' at '$target'"
-      VERSION_CONTROL=t ln -sbv "$file" "$target"
-    fi
-  done
+  # ignore special files
+  if [[ "$file_name" == ".git" || "$file_name" == ".gitignore" ]]; then
+    continue
+  fi
 
-  echo_success "Finished linking dotfiles"
-}
+  target="$HOME/.config/$file_name"
+
+  # back up target dir if it exists
+  if [[ -d "$target" ]]; then
+    VERSION_CONTROL=t mv -bTv "$target" "${target}.backup"
+  fi
+
+  # create symlink
+  set +e
+  symlink_target=$(readlink "$target")
+  set -e
+  if [[ "$symlink_target" == "$file" ]]; then
+    echo "Symlink '$target' -> '$symlink_target' already exists!"
+  else
+    echo "Creating symlink for '$file' at '$target'"
+    VERSION_CONTROL=t ln -sbv "$file" "$target"
+  fi
+done
+
+echo_success "Finished linking dotfiles"
