d5328b4918310fa3784185f6992a503fe90b40aa
Bach Nguyen <69bnguyen@gmail.com>
2025-10-12 18:56:44 +0800

    prompt user for confirmation when linking dotfiles and configs

--- dotfiles/symlink-dotfiles.sh
+++ dotfiles/symlink-dotfiles.sh
@@ -10,71 +10,87 @@ echo_warning "This script will symlink all dotfiles in $dotfiles_dir to the home
 echo_warning "All directories in $dotfiles_dir/.config/ will also be symlinked to $HOME/.config/."
 echo_warning "All files and directories will be backed up (with versioning) by default."
 
-read -rp "Proceed? [y/n]: " user_input
+# symlink dotfiles
+read -rp "Link dotfiles? [y/n]: " user_input
+link_dotfiles=false
 case "$user_input" in
-"y") ;;
-"n") return 1 ;;
+"y") link_dotfiles=true ;;
+"n") ;;
 *)
   echo_error "Invalid input: '$user_input'."
   return 1
   ;;
 esac
 
-# symlink dotfiles
-echo "Linking dotfiles..."
+if $link_dotfiles; then
+  echo "Linking dotfiles..."
 
-for file in "$dotfiles_dir"/.*; do
-  file_name=$(basename "$file")
+  for file in "$dotfiles_dir"/.*; do
+    file_name=$(basename "$file")
 
-  # Ignore folders & special files
-  if [[ -d "$file" ]] || [[ "$file_name" == ".gitignore" ]]; then
-    continue
-  fi
-
-  target="$HOME/$file_name"
-
-  # create symlink with versioned backup
-  set +e
-  symlink_target=$(readlink "$target")
-  set -e
-  if [[ "$symlink_target" == "$file" ]]; then
-    echo "Symlink '$target' -> '$symlink_target' already exists!"
-  else
-    echo "Creating symlink for '$file' at '$target'"
-    VERSION_CONTROL=t ln -sbv "$file" "$target"
-  fi
-done
-
-# symlink dirs in .config/
-if [[ ! -d "$HOME/.config" ]]; then
-  mkdir "$HOME/.config"
-fi
-
-for file in "$dotfiles_dir"/.config/*; do
-  file_name=$(basename "$file")
-
-  # ignore special files
-  if [[ "$file_name" == ".git" || "$file_name" == ".gitignore" ]]; then
-    continue
-  fi
-
-  target="$HOME/.config/$file_name"
-
-  # create symlink
-  set +e
-  symlink_target=$(readlink "$target")
-  set -e
-  if [[ "$symlink_target" == "$file" ]]; then
-    echo "Symlink '$target' -> '$symlink_target' already exists!"
-  else
-    # back up target dir if it exists
-    if [[ -d "$target" ]]; then
-      VERSION_CONTROL=t mv -bTv "$target" "${target}.backup"
+    # Ignore folders & special files
+    if [[ -d "$file" ]] || [[ "$file_name" == ".gitignore" ]]; then
+      continue
     fi
 
-    echo "Creating symlink for '$file' at '$target'"
-    VERSION_CONTROL=t ln -sbv "$file" "$target"
+    target="$HOME/$file_name"
+
+    # create symlink with versioned backup
+    set +e
+    symlink_target=$(readlink "$target")
+    set -e
+    if [[ "$symlink_target" == "$file" ]]; then
+      echo "Symlink '$target' -> '$symlink_target' already exists!"
+    else
+      echo "Creating symlink for '$file' at '$target'"
+      VERSION_CONTROL=t ln -sbv "$file" "$target"
+    fi
+  done
+fi
+
+# symlink dirs in .config/
+read -rp "Link $HOME/.config? [y/n]: " user_input
+link_configs=false
+case "$user_input" in
+"y") link_configs=true ;;
+"n") ;;
+*)
+  echo_error "Invalid input: '$user_input'."
+  return 1
+  ;;
+esac
+
+if $link_configs; then
+  if [[ ! -d "$HOME/.config" ]]; then
+    mkdir "$HOME/.config"
   fi
-done
+
+  for file in "$dotfiles_dir"/.config/*; do
+    file_name=$(basename "$file")
+
+    # ignore special files
+    if [[ "$file_name" == ".git" || "$file_name" == ".gitignore" ]]; then
+      continue
+    fi
+
+    target="$HOME/.config/$file_name"
+
+    # create symlink
+    set +e
+    symlink_target=$(readlink "$target")
+    set -e
+    if [[ "$symlink_target" == "$file" ]]; then
+      echo "Symlink '$target' -> '$symlink_target' already exists!"
+    else
+      # back up target dir if it exists
+      if [[ -d "$target" ]]; then
+        VERSION_CONTROL=t mv -bTv "$target" "${target}.backup"
+      fi
+
+      echo "Creating symlink for '$file' at '$target'"
+      VERSION_CONTROL=t ln -sbv "$file" "$target"
+    fi
+  done
+fi
 
 echo_success "Finished linking dotfiles"
